edca411613c772a81185691eadde28c85c1b0722
aditya26-gif <76239749+aditya26-gif@users.noreply.github.com>
2022-03-21 11:39:13 +0530

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,21 @@
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+      apiKey: "AIzaSyDeZexclN7an39OrBwyd5KBlQsn8oFModE",
+      authDomain: "classtest-b805d.firebaseapp.com",
+      projectId: "classtest-b805d",
+      storageBucket: "classtest-b805d.appspot.com",
+      messagingSenderId: "475624976651",
+      appId: "1:475624976651:web:a542aa09bb4e524bf1b20e"
+    };
+  
+    // Initialize Firebase
+    const app = initializeApp(firebaseConfig);
+
+function getData() {firebase.database().ref("/").on('value', function(snapshot) {document.getElementById("output").innerHTML = "";snapshot.forEach(function(childSnapshot) {childKey  = childSnapshot.key;
+       Room_names = childKey;
+      //Start code
+
+      //End code
+      });});}
+getData();
